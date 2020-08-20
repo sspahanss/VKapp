1bf0bcd3d12150dcf0160ba6551a2b5fef404d69
Parcifal <scar1987@gmail.com>
2020-08-21 00:02:31 +0800

    переделал все с первого урока

---
+++ VKapp/TabBarViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TabBarViewController.swift
+//  VKapp
+//
+//  Created by Павел on 20.08.2020.
+//  Copyright © 2020 Павел. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+
+
+
+}
